6db3a1211970d0f7ea0718808b4db7361001aac9
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-05 08:08:13 -0800

    IOCE minSubArrayLength

---
+++ ToyProbs/minSubArrayLength.js
@@ -0,0 +1,31 @@
+/*
+  accepts two params :
+  - an array of positive intergers
+  - positive integer
+
+  returns the minimal length of a contiguous subarray of which the sum
+  is greater than or equal to the integer passed to the function
+
+  if none is found return 0
+
+I :
+- array
+- integer
+
+O : integer (minimum number of consecutive array elements whose sum is greater
+    than or equal to input integer)
+E :
+- args : none, one, more than 2, one or both not an array or integer
+- input intger = 0
+- input array length = 0
+C :
+- O(n) linear time
+- O(1) constant space
+
+
+STRATEGY
+
+
+APPROXIMATION
+
+*/
