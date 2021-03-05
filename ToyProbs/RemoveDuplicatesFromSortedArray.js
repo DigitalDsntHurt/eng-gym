99cb9b3bcd640d807b249862352ed0b6139dfa76
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-05 10:03:46 -0800

    Scaffold remove duplicates

---
+++ ToyProbs/RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,21 @@
+// Given a sorted array nums, remove the duplicates in-place
+// such that each element appears only once and
+// returns the new length.
+//
+// Do not allocate extra space for another array,
+// you must do this by modifying the input array in-place with
+// O(1) extra memory.
+
+/*
+  I : sorted array
+  O : number
+  E : ignore for now
+  C : O(n) time | O(1) space
+*/
+
+const removeDuplicates = function(nums) {
+
+};
+
+console.log(removeDuplicates([1,1,2])) // 2
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])) // 5
