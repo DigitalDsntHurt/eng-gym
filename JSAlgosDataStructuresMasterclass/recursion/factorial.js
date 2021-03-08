2e47a45f6afb505eb49f2f266dc31ea901de476e
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-08 08:32:38 -0800

    Solve factorial recursively

--- JSAlgosDataStructuresMasterclass/recursion/factorial.js
+++ JSAlgosDataStructuresMasterclass/recursion/factorial.js
@@ -1,17 +1,18 @@
 
-// ITERATIVE
-const factorial = (n) => {
-  let fac = 1;
-  while (n > 1) {
-    fac *= n;
-    n--;
-  }
-  return fac;
-};
-console.log(factorial(8)) // 40320
+// // ITERATIVE
+// const factorial = (n) => {
+//   let fac = 1;
+//   while (n > 1) {
+//     fac *= n;
+//     n--;
+//   }
+//   return fac;
+// };
+// console.log(factorial(8)) // 40320
 
 // RECURSIVE
-// const factorial = (n) => {
-//
-// };
+const factorial = (n) => {
+  if (n === 1) return n;
+  return n * factorial(n - 1);
+};
 console.log(factorial(8)) // 40320
