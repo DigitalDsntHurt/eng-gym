76f34ef3f4d710e745f2320df64174c39795c578
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-08 08:27:02 -0800

    Refactor base case for sumRange. Setup factorial

---
+++ JSAlgosDataStructuresMasterclass/recursion/factorial.js
@@ -0,0 +1,12 @@
+
+// ITERATIVE
+const factorial = (n) => {
+
+};
+factorial(8)
+
+// RECURSIVE
+// const factorial = (n) => {
+//
+// };
+// factorial(8);
--- JSAlgosDataStructuresMasterclass/recursion/sumRange.js
+++ JSAlgosDataStructuresMasterclass/recursion/sumRange.js
@@ -3,7 +3,7 @@
 */
 
 const sumRange = (n) => {
-  if (n === 0) { return 0; }
+  if (n === 1) { return 1; }
   return n + sumRange(n - 1)
 };
 
