56929989c1179c851fef95932ca4cac48834b9b6
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-08 08:55:15 -0800

    Solve collectAllOdds with helper method recursion pattern

--- JSAlgosDataStructuresMasterclass/recursion/collectAllOdds.js
+++ JSAlgosDataStructuresMasterclass/recursion/collectAllOdds.js
@@ -1,13 +1,20 @@
 
 
 // Pure Recursion
-// const collectAllOdds = (arr) => {
-//
-// };
-// console.log(collectAllOdds([1,2,3,4,5,6,7,8])) // [1,3,5,7]
-
-// Helper Method Recursion
 const collectAllOdds = (arr) => {
-
+  
 };
 console.log(collectAllOdds([1,2,3,4,5,6,7,8])) // [1,3,5,7]
+
+// // Helper Method Recursion
+// const collectAllOdds = (arr) => {
+//   let odds = [];
+//   const isOdd = (nums) => {
+//     if (nums.length === 0) { return; }
+//     if (nums[0] % 2 !== 0) { odds.push(nums[0]); }
+//     isOdd(nums.slice(1))
+//   }
+//   isOdd(arr);
+//   return odds;
+// };
+// console.log(collectAllOdds([1,2,3,4,5,6,7,8])) // [1,3,5,7]
