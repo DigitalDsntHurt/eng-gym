efbcac71b377962b3901eadbab882271d54729fb
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-12 22:08:52 -0800

    scaffold linkedlist

---
+++ DataStructures/linkedList.js
@@ -0,0 +1,32 @@
+class LinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+    }
+
+    addToHead() {
+
+    }
+
+    removeFromTail() {
+
+    }
+
+    find() {
+
+    }
+
+    contains() {
+
+    }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+let first = new Node(4);
+console.log(first);
