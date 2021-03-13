42d2c5fa35ed9d31b9179a1b11e49c18c7270ded
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-12 22:21:38 -0800

    Implement and test linked list add to head function

--- DataStructures/linkedList.js
+++ DataStructures/linkedList.js
@@ -1,11 +1,14 @@
 class LinkedList {
-    constructor() {
-      this.head = null;
-      this.tail = null;
+    constructor(value) {
+      let newNode = new Node(value);
+      this.head = newNode;
+      this.tail = newNode;
     }
 
-    addToHead() {
-
+    addToHead(value) {
+      let newNode = new Node(value);
+      newNode.next = this.head;
+      this.head = newNode;
     }
 
     removeFromTail() {
@@ -28,5 +31,17 @@ class Node {
   }
 }
 
-let first = new Node(4);
-console.log(first);
+// let first = new Node(4);
+// console.log(first);
+
+let list = new LinkedList(2);
+console.log(list);
+list.addToHead(4);
+console.log(list); // LinkedList { head: NODE{ value: 4, next: Node{ value: 2, next: null } }, tail: next: Node{ value: 2, next: null }}
+console.log(list.head) // NODE{ value: 4, next: Node{ value: 2, next: null } }
+console.log(list.tail) // Node{ value: 2, next: null }
+list.addToHead(6);
+list.addToHead(8);
+console.log(list.head) // NODE{ value: 8, next: Node{ value: 6, next: null } }
+console.log(list.tail) // Node{ value: 2, next: null }
+console.log(list.head.next.next) // Node{ value: 4, next: Node{ value: 2, next: null }}
