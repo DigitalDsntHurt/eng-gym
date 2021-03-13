aeb40f37214c00595c564c60b8c04461c083d001
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-12 22:31:10 -0800

    Write tests and pseudocode for LinkedLists removeFromTail() method

--- DataStructures/linkedList.js
+++ DataStructures/linkedList.js
@@ -3,24 +3,35 @@ class LinkedList {
       let newNode = new Node(value);
       this.head = newNode;
       this.tail = newNode;
+      this.length = 1;
     }
 
     addToHead(value) {
       let newNode = new Node(value);
       newNode.next = this.head;
       this.head = newNode;
+      this.length++;
     }
 
     removeFromTail() {
+      // start at head
+      // iterate over each node
+        // if currentNode.next = tail
+          // set tail to currentNode
+          // set currentNode.next to null
+      this.length--;
+    }
+
+    find(value) {
 
     }
 
-    find() {
+    contains(value) {
 
     }
 
-    contains() {
-
+    length() {
+      return this.length;
     }
 }
 
@@ -45,3 +56,8 @@ list.addToHead(8);
 console.log(list.head) // NODE{ value: 8, next: Node{ value: 6, next: null } }
 console.log(list.tail) // Node{ value: 2, next: null }
 console.log(list.head.next.next) // Node{ value: 4, next: Node{ value: 2, next: null }}
+console.log(list.head.next.next.next) // Node{ value: 2, next: null }
+console.log(list.tail) // Node{ value: 2, next: null }
+list.removeFromTail();
+console.log(list.tail) // Node{ value: 4, next: null }
+console.log(list.head.next.next) // Node{ value: 4, next: null }
