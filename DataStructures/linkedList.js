bed9d49328b40a905146e06ffd211ad09a89ca40
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-13 20:56:29 -0800

    Implement linkedList pop() in linear time

--- DataStructures/linkedList.js
+++ DataStructures/linkedList.js
@@ -18,7 +18,23 @@ class LinkedList {
     }
 
     pop() {
-
+      // instantiate var current node = this.tail.next
+      // instantiate var prev node = this.tail
+      // loop while currentNode !== head
+        // currentNode = currentNode.next
+        // prevNode = prevNode.next
+      // this.head = prevNode
+      // this.head.next = null
+      // decrement length
+      let prevNode = this.tail;
+      let currentNode = this.tail.next;
+      while (currentNode !== this.head) {
+        prevNode = prevNode.next;
+        currentNode = currentNode.next;
+      }
+      this.head = prevNode;
+      this.head.next = null;
+      this.length--;
     }
 
     find(value) {
@@ -49,6 +65,15 @@ console.log(list);
 list.push(2);
 // console.log(list); // LinkedList { head: Node{ value: 2, next: null }, tail: Node{ value: 2, next: null }, length: 1 }
 list.push(4);
-console.log(list); // LinkedList { head: Node{ value: 4, next: Node{ value: 2, next: null } }, tail: next: Node{ value: 2, next: null }}
+list.push(6);
+list.push(8);
+console.log(list);
+console.log(list.length);
+console.log('===')
+list.pop();
+console.log(list);
+console.log(list.tail.next);
+console.log(list.tail.next.next);
+console.log(list.length);
 // console.log(list.head) // NODE{ value: 4, next: Node{ value: 2, next: null } }
 // console.log(list.tail) // Node{ value: 2, next: null }
