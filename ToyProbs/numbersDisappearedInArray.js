57a8852a97ae14bdf70f669e16f711b555cba823
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-18 07:58:43 -0700

    Setup findDisappearedNumbers

---
+++ ToyProbs/numbersDisappearedInArray.js
@@ -0,0 +1,23 @@
+/*
+  FROM : https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+  given an array of integers where
+  - array is of length n
+  - all array elements are between 1 and n inclusive
+
+  find all elements of the inclusive range 1->n that do not appear in the array
+
+  I : array of integers
+  O : array of integers
+  E : -
+  C :
+  - O(1) space
+  - O(n) time
+*/
+
+
+const findDisappearedNumbers = (arr) => {
+
+};
+
+console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1])) // [5,6]
