e870d7a75353d834beaf3275c99fc74eb0658bad
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-19 16:25:18 -0700

    Prepare hashTable

---
+++ DataStructures/hashTable.js
@@ -0,0 +1,12 @@
+/*
+ - what is a hash table
+
+ - what is a hashing algorithm
+
+ - what makes a good hashing algorithm
+
+ - how collisions occur in a hash table
+
+ - handle collisions using separate chaining or linear probing
+ 
+*/
