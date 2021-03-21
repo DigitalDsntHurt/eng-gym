1bccee9401f44e9feb52d741ffafb9fdaefe367c
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-21 14:32:13 -0700

    Write HashTable scaffold

--- DataStructures/hashTable.js
+++ DataStructures/hashTable.js
@@ -12,3 +12,25 @@
  // BASIC HASHTABLE class
  https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/8344862#content
 */
+
+class HashTable {
+  constructor() {
+
+  }
+
+  insert() {
+
+  }
+
+  findByKey() {
+
+  }
+
+  remove() {
+
+  }
+
+  contains() {
+    
+  }
+}
