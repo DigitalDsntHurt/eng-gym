6d34deca071e4ebe8e4df2773bd332e1a4a19b28
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-03-22 09:28:51 -0700

    Write hash function for HashTable starter code

--- DataStructures/hashTable.js
+++ DataStructures/hashTable.js
@@ -14,8 +14,19 @@
 */
 
 class HashTable {
-  constructor() {
+  constructor(size=53) {
+    this.keyMap = new Array(size);
+  }
 
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
   }
 
   insert() {
@@ -31,6 +42,6 @@ class HashTable {
   }
 
   contains() {
-    
+
   }
 }
