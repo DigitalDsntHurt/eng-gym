ff60d62c85113f3e85b7c09930737e52599a06b4
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-04-07 10:18:12 -0700

    Solve async-word-count with relative file paths using nodes path.normalize

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -30,7 +30,7 @@ var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
 module.exports = getTotalWordCount;
 // // ABSOLUTE PATH APPROACH
 // const fp1 = '/Users/attack/Eng/hrsprints/hr-sfo134-self-assessment-week-04-v7/async-word-count/files/fileOne.txt';
-const fp2 = '/Users/attack/Eng/hrsprints/hr-sfo134-self-assessment-week-04-v7/async-word-count/files/fileTwo.txt';
+// const fp2 = '/Users/attack/Eng/hrsprints/hr-sfo134-self-assessment-week-04-v7/async-word-count/files/fileTwo.txt';
 
 // // REQUIRE APPROACH ~ does not work
 // const fp1 = require('./files/fileOne.txt')
@@ -38,8 +38,10 @@ const fp2 = '/Users/attack/Eng/hrsprints/hr-sfo134-self-assessment-week-04-v7/as
 
 // // RELATIVE APPROACH ~ does not work
 // const fp1 = path.relative('files', 'fileOne.txt')
+
+// // NORMALIZE APPROACH ~ appears to work
 const fp1 = path.normalize('./files/fileOne.txt')
-console.log(fp1)
+const fp2 = path.normalize('./files/fileTwo.txt')
 getTotalWordCount(
   fp1, fp2, (err, tc) => {
     if (err) { throw err; }
