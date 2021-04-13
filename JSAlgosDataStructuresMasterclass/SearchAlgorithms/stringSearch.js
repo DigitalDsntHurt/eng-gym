10f03affba872620c39fc25374b5c5427f711071
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-04-13 11:38:46 -0700

    Solve naive substring search

--- JSAlgosDataStructuresMasterclass/SearchAlgorithms/stringSearch.js
+++ JSAlgosDataStructuresMasterclass/SearchAlgorithms/stringSearch.js
@@ -4,8 +4,41 @@
 // in the chars do match keep going
 // return count
 
-const stringSearch = (string, substring) => {
-  
+// const stringSearch = (string, substring) => {
+//   let matches = 0;
+//   // for (char of string) {
+//   //   for (subchar of substring) {
+//   //     if (char !== subchar) { break; }
+//   //   }
+//   // }
+//   for (let i = 0; i < string.length; i++) {
+//     // let char = string[i];
+//     for (let j = 0; j < substring.length; j++) {
+//       // console.log(string[i], string[j])
+//       // let subchar = substring[j];
+//       if (string[i] !== substring[j]) {
+//
+//         // break;
+//       }
+//       if (j === substring.length - 1) {
+//         console.log(j, substring.length - 1)
+//         // console.log(string[i], string[j])
+//         matches++;
+//       }
+//     }
+//   }
+//   return matches;
+// };
+
+const stringSearch = (long, short) => {
+  let matches = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (var j = 0; j < short.length; j++) {
+      if (long[i+j] !== short[j]) { break; }
+      if (j === short.length - 1) { matches++; }
+    }
+  }
+  return matches;
 };
 
 console.log(stringSearch('oompaloompa', 'loom')) // 1
