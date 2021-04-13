81154583bcf4ebc6949675e4b67d8f524b53cae4
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-04-13 11:50:21 -0700

    Refactor binary search

--- JSAlgosDataStructuresMasterclass/SearchAlgorithms/binarySearch.js
+++ JSAlgosDataStructuresMasterclass/SearchAlgorithms/binarySearch.js
@@ -1,13 +1,40 @@
-const binarySearch = (array, target) => {
+// const binarySearch = (array, target) => {
+//   let front = 0;
+//   let back = array.length - 1;
+//   let mid = Math.floor((front + back) / 2);
+//   while (target !== array[mid] && front <= back) {
+//     if (target > array[mid]) { front = mid + 1; }
+//     if (target < array[mid]) { back = mid - 1; }
+//     mid = Math.floor((front + back) / 2);
+//   }
+//   return array[mid] === target ? array[mid] : -1;
+// }
+
+// // SECOND BLIND TRY
+// function binarySearch(array, target){
+//   let front = 0;
+//   let back = array.length - 1;
+//   let midpoint = Math.floor((front + back) / 2);
+//   while (target !== array[midpoint] && front <= back) {
+//     if (target > array[midpoint]) { front = midpoint + 1; }
+//     if (target < array[midpoint]) { back = midpoint - 1; }
+//     midpoint = Math.floor((front + back) / 2);
+//   }
+//   return target === array[midpoint] ? midpoint : -1;
+// }
+
+// ONCE MORE
+function binarySearch(array, target){
   let front = 0;
   let back = array.length - 1;
-  let mid = Math.floor((front + back) / 2);
-  while (target !== array[mid] && front <= back) {
-    if (target > array[mid]) { front = mid + 1; }
-    if (target < array[mid]) { back = mid - 1; }
-    mid = Math.floor((front + back) / 2);
+  let midpoint = Math.floor((front + back) / 2);
+  while (front <= back) {
+    if (target === array[midpoint]) { return midpoint; }
+    if (target > array[midpoint]) { front = midpoint + 1; }
+    if (target < array[midpoint]) { back = midpoint - 1; }
+    midpoint = Math.floor((front + back) / 2);
   }
-  return array[mid] === target ? array[mid] : -1;
+  return -1;
 }
 
 
