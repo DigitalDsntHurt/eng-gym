a2fdfa60bd0395b70727af024437fb7ac120f744
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-04-21 07:50:39 -0700

    IOCE bracket challenge

---
+++ TakeHomeChallenges/bracketPairs.js
@@ -0,0 +1,39 @@
+/*
+In the language you're most comfortable with, write a function that
+takes a string and returns a boolean. The function makes sure that
+pairs of brackets are matching and properly nested:
+- every "{" should be closed by a corresponding "}",
+- every "[" closed by a "]",
+- every left parenthesis by a right parenthesis;
+
+it returns true if all are matched,
+false if there is a mismatch.
+
+In addition, the bar character "|" closes itself (like Ruby's iteration variable for a block).
+
+Note that a string like "{ [ } ]" should return false.
+
+I : string
+O : boolean
+E : empty string, non-string, string of len 1
+C : n/a
+
+THOUGHTS
+- there are three types of bracket: ( { [
+- there need to be an even number of each bracket type
+- a bracket opened WITHIN any other needs to be closed within its parent
+
+STRATEGY
+- remove all characters that aren't brackets
+- use two pointers strategy starting at first and last character
+- - if front pointer and back pointer point to different bracket types
+- - - - early return false
+- - else
+- - - - increment front pointer and decrement back pointer
+
+*/
+ const hasValidBrackets = (string) => {
+   if (typeof string !== 'string' || string.length === 0 || string === undefined) { return null; }
+   let front = 0;
+   let back = string.length - 1;
+ };
