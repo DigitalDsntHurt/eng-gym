4c9e8737772ad0fda78cd008dd1e6d342514d792
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-04-21 08:13:59 -0700

    Edge cases

--- TakeHomeChallenges/bracketPairs.js
+++ TakeHomeChallenges/bracketPairs.js
@@ -32,22 +32,38 @@ STRATEGY
 - - - - increment front pointer and decrement back pointer
 
 */
- const hasValidBrackets = (string) => {
-   const brackets = '(){}[]';
-   const regexp = new RegExp(/{|}|\[|\]|\(|\)/, 'g')
-   return regexp.test(string);
-   // if (typeof string !== 'string' || string === undefined || ) { return null; }
-   // if (string.length === 0 || )
-   //
-   // for (let char of string) {
-   //   if (brackets)
-   // }
-   //
-   // let front = 0;
-   // let back = string.length - 1;
- };
+const hasValidBrackets = (string) => {
+ // const brackets = '(){}[]';
+ const regexp = new RegExp(/{|}|\[|\]|\(|\)/, 'g')
+ // return regexp.test(string);
+ if (typeof string !== 'string' || string === undefined) { return null; }
+ if (string.length === 0 || !regexp.test(string)) { return true; }
 
- console.log(hasValidBrackets('hello moto'))
- console.log(hasValidBrackets('hello {moto'))
- console.log(hasValidBrackets('hello moto}'))
- console.log(hasValidBrackets('hello [moto'))
+ let justTheBrackets = '';
+ for (let char of string) {
+   if (regexp.test(char)) { justTheBrackets += char; }
+ }
+
+ return justTheBrackets;
+ //
+ // let front = 0;
+ // let back = string.length - 1;
+};
+
+// test not string returns null
+console.log(hasValidBrackets(99)) // null
+// test no argument returns null
+console.log(hasValidBrackets()) // null
+// test empty string return true
+console.log(hasValidBrackets('')) // true
+// test no brackets returns true
+console.log(hasValidBrackets('hello moto')) // true
+// console.log(hasValidBrackets('hello {moto'))
+// console.log(hasValidBrackets('hello moto}'))
+// console.log(hasValidBrackets('hello [moto'))
+/*
+
+
+
+test bracket skinner
+*/
