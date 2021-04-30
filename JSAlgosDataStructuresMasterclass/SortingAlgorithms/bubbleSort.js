fe34d1cfb05b1fe74af3890eb9645b33e577aafd
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-04-30 09:33:01 -0500

    outline bubblesort

---
+++ JSAlgosDataStructuresMasterclass/SortingAlgorithms/bubbleSort.js
@@ -0,0 +1,10 @@
+const bubbleSort = (arr) => {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    for (var j = 0; j = i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        // swap values
+      }
+    }
+  }
+  return arr;
+}
