4771708b65b565373c20f1a320f4f04e2d53d72a
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-04-30 09:55:59 -0500

    Implement lightly optimized bubble sort (no longer performs extra comparisons

--- JSAlgosDataStructuresMasterclass/SortingAlgorithms/bubbleSort.js
+++ JSAlgosDataStructuresMasterclass/SortingAlgorithms/bubbleSort.js
@@ -14,28 +14,29 @@ const swapValues = (arr, idx1, idx2) => {
 
 
 
-// naive nested loop solution
-const bubbleSort = (arr) => {
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr.length; j++) {
-        if (arr[j] > arr[j +1]) {
-          swapValues(arr, j, j+1);
-        }
-    }
-  }
-  return arr;
-}
-
+// // naive nested loop solution
 // const bubbleSort = (arr) => {
-//   for (var i = arr.length - 1; i >= 0; i--) {
-//     for (var j = 0; j = i - 1; j++) {
-//       if (arr[j] > arr[j+1]) {
-//         swapValues(arr, j, j+1);
-//       }
+//   for (var i = 0; i < arr.length; i++) {
+//     for (var j = 0; j < arr.length; j++) {
+//         if (arr[j] > arr[j +1]) {
+//           swapValues(arr, j, j+1);
+//         }
 //     }
 //   }
 //   return arr;
 // }
+
+// slightly optimized solution... does not spend extra, unnecessary iterations
+const bubbleSort = (arr) => {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j +1]) {
+        swapValues(arr, j, j+1);
+      }
+    }
+  }
+  return arr;
+}
 
 console.log(bubbleSort([3,33,54,2,1])) // [1, 2, 3, 33, 54]
 console.log(bubbleSort([3,-4,-5])) // [-5, -4, 3]
