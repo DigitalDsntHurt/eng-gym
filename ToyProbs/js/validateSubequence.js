7731b29722741f1bc97496a21ec636ea83d56a30
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-05-04 15:04:46 -0500

    solve validateSubsequence with forloop approach

--- ToyProbs/js/validateSubequence.js
+++ ToyProbs/js/validateSubequence.js
@@ -1,13 +1,25 @@
+// // WHILE LOOP APPROACH
+// function isValidSubsequence(array, sequence) {
+// 	let arrPointer = 0;
+// 	let seqPointer = 0;
+// 	while (seqPointer < sequence.length && arrPointer < array.length) {
+// 		if (array[arrPointer] === sequence[seqPointer]) {
+// 			seqPointer++;
+// 		}
+// 		arrPointer++;
+// 	}
+// 	return seqPointer === sequence.length;
+// }
+
+// FOR LOOP APPROACH
 function isValidSubsequence(array, sequence) {
-	let arrPointer = 0;
-	let seqPointer = 0;
-	while (seqPointer < sequence.length && arrPointer < array.length) {
-		if (array[arrPointer] === sequence[seqPointer]) {
-			seqPointer++;
+  sequencePointer = 0;
+	for (let arrayPointer = 0; arrayPointer < array.length; arrayPointer++) {
+		if (array[arrayPointer] === sequence[sequencePointer]) {
+			sequencePointer++;
 		}
-		arrPointer++;
 	}
-	return seqPointer === sequence.length;
+	return sequencePointer === sequence.length;
 }
 
 console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])); // true
