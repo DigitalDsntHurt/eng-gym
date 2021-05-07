cc39428caf1a71d7aa327467f51a88df03e5424e
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-05-06 20:18:30 -0500

    three solutions to twoNumberSum

--- ToyProbs/js/twoNumberSum.js
+++ ToyProbs/js/twoNumberSum.js
@@ -1,10 +1,43 @@
-// o(n**2) time, o(1) space
+// // o(n**2) time, o(1) space
+// function twoNumberSum(array, targetSum) {
+// 	for (let i = 0; i < array.length; i++) {
+// 		for (let j = i + 1; j < array.length; j++) {
+// 			if (array[i] + array[j] === targetSum) {
+// 				return [array[i], array[j]];
+// 			}
+// 		}
+// 	}
+// 	return [];
+// }
+
+// // O(n) time, O(n) space
 function twoNumberSum(array, targetSum) {
-	for (let i = 0; i < array.length; i++) {
-		for (let j = i + 1; j < array.length; j++) {
-			if (array[i] + array[j] === targetSum) {
-				return [array[i], array[j]];
-			}
+  let frequencies = {};
+	for (let num of array) {
+		let lookingFor = targetSum - num;
+		if (frequencies[lookingFor]) { return [num, lookingFor]; }
+		frequencies[num] = true;
+	}
+	return [];
+}
+
+// // O(NlogN) time, O(1) space
+// two pointers solution
+function twoNumberSum(array, targetSum) {
+	array = array.sort((a,b) => a - b);
+
+	let left = 0;
+	let right = array.length - 1;
+	while (right > left) {
+		const sum = array[left] + array[right];
+		if (sum === targetSum) {
+			return [array[left], array[right]];
+		}
+		if (sum > targetSum) {
+			right--;
+		}
+		if (sum < targetSum) {
+			left++;
 		}
 	}
 	return [];
