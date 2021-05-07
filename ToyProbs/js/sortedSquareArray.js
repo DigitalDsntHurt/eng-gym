1819e48546229a73f1390ada4aa129ee09c05ed6
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-05-07 08:28:49 -0500

    naive solution sortedSquareArray

--- ToyProbs/js/sortedSquareArray.js
+++ ToyProbs/js/sortedSquareArray.js
@@ -1,6 +1,19 @@
-function sortedSquaredArray(array) {
+/*
+  // STRATEGY #1
+  iterate over each item in the array
+    calculate item's square
+    replace current array item with squared array item
+  return array
+*/
 
+function sortedSquaredArray(array) {
+  for (var i = 0; i < array.length; i++) {
+    array[i] = array[i] ** 2;
+  }
+  return array;
 }
+
+
 
 console.log(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])) // [1,4,9,25,36,64,81]
 console.log(sortedSquaredArray([0])) // [0]
