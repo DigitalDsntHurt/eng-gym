aa951ef66486a6055263fa666d4318bb216e2c7c
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-05-07 17:24:48 -0500

    Complete optimized solution for sortedSquareArray.js

--- ToyProbs/js/sortedSquareArray.js
+++ ToyProbs/js/sortedSquareArray.js
@@ -40,22 +40,28 @@ function sortedSquaredArray(array) {
   while (small <= large) {
     let smallVal = array[small];
     let largeVal = array[large];
-    let max = Math.max(Math.abs(smallVal), Math.abs(largeVal))
-    if (max === smallVal) {
+    if (Math.abs(smallVal) > Math.abs(largeVal)) {
       sortedSquares.unshift(smallVal * smallVal);
       small++;
     } else {
       sortedSquares.unshift(largeVal * largeVal);
       large--;
     }
+    console.log(sortedSquares);
   }
   return sortedSquares;
 }
 
 console.log(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])) // [1,4,9,25,36,64,81]
+console.log('\n************\n\n\n')
 console.log(sortedSquaredArray([0])) // [0]
+console.log('\n************\n\n\n')
 console.log(sortedSquaredArray([10])) // [100]
+console.log('\n************\n\n\n')
 console.log(sortedSquaredArray([-1])) // [1]
+console.log('\n************\n\n\n')
 console.log(sortedSquaredArray([-5, -4, -3, -2, -1])) // [1,4,9,16,25]
+console.log('\n************\n\n\n')
 console.log(sortedSquaredArray([-10, -5, 0, 5, 10])) // [0, 25, 25, 100, 100]
+console.log('\n************\n\n\n')
 // console.log(sortedSquaredArray([-50, -13, -2, -1, 0, 0, 1, 1, 2, 3, 19, 20]))
