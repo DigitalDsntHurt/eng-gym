5785f0beef8d1f6d9e39f3412b33047f26851451
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-05-07 08:23:42 -0500

    Write tests for sortedSquareArray

--- ToyProbs/js/sortedSquareArray.js
+++ ToyProbs/js/sortedSquareArray.js
@@ -1,10 +1,11 @@
 function sortedSquaredArray(array) {
-  // iterate over each item in array
-	  // square the value and replace current item
-	// return array
 
-	for(let i = 0; i < array.length; i++) {
-		array[i] = array[i]*array[i];
-	}
-  return array.sort((a,b) => a > b );
 }
+
+console.log(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])) // [1,4,9,25,36,64,81]
+console.log(sortedSquaredArray([0])) // [0]
+console.log(sortedSquaredArray([10])) // [100]
+console.log(sortedSquaredArray([-1])) // [1]
+console.log(sortedSquaredArray([-5, -4, -3, -2, -1])) // [1,4,9,16,25]
+console.log(sortedSquaredArray([-10, -5, 0, 5, 10])) // [0, 25, 25, 100, 100]
+// console.log(sortedSquaredArray([-50, -13, -2, -1, 0, 0, 1, 1, 2, 3, 19, 20]))
