2e78f08e79d5a632c99bd0bc1ef5f6bae32fc1fc
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-05-12 16:27:12 -0400

    pseudocode

--- ToyProbs/js/nonConstructibleChange.js
+++ ToyProbs/js/nonConstructibleChange.js
@@ -1,3 +1,11 @@
+// sort the input array
+// initialize a changeSum variable to 0
+// for each item in the input array
+  // if current item > changeSum + 1
+    // return changeSum + 1
+  // else changeSum += current item
+// if we get to the end of the loop return changeSum + 1
+
 const nonConstructibleChange = coins => {
 
 };
