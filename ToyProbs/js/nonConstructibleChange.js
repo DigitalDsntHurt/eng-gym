27a1764c27a779b62cb0ffdd36ca0a67d3a8bd9a
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-05-12 16:31:02 -0400

    first working solution

--- ToyProbs/js/nonConstructibleChange.js
+++ ToyProbs/js/nonConstructibleChange.js
@@ -7,7 +7,13 @@
 // if we get to the end of the loop return changeSum + 1
 
 const nonConstructibleChange = coins => {
-
+  coins = coins.sort((a, b) => ( a - b ))
+  let changeSum = 0;
+  for (let i = 0; i < coins.length; i++) {
+    if (coins[i] > changeSum + 1) { return changeSum + 1; }
+    changeSum += coins[i];
+  }
+  return changeSum + 1;
 };
 
 console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22])); // 20
