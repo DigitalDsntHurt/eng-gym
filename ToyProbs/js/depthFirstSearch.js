6783c4d10acdf3e3ee1799239a16b09b38b184c1
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-05-21 16:39:36 -0400

    solve dfs

---
+++ ToyProbs/js/depthFirstSearch.js
@@ -0,0 +1,54 @@
+/*
+I : empty array
+O : array of node names in dfs order
+E :
+C :
+
+STRATEGY
+	initialize a nodes var to an empty array
+	write recursive function
+	call recursive function
+	return nodes array
+
+	recursive function
+		add node.name to nodes arr
+		if node.children is empty
+		  return
+		else
+			iterate over each item in node.children
+				make recursive call
+*/
+
+// Do not edit the class below except
+// for the depthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+		const populateNodesArray = (node) => {
+			array.push(node.name);
+			if (node.children.length > 0) {
+				node.children.forEach(node => {
+					populateNodesArray(node);
+				})
+			} else {
+				return;
+			}
+		}
+		populateNodesArray(this);
+		return array;
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
